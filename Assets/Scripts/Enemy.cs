eb84df6d5ab8cf5cdf3bf80f31d19464260db195
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2019-06-14 12:05:08 +1000

    Added scripts, prefabs, the game and other stuff.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform M_Player;
+    public GameObject dieScreen;
+    public AudioSource dieSound;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = M_Player.position;
+    }
+
+    void OnTriggerEnter( Collider other)
+    {
+        if(other.name == "Player")
+        {
+            //Destroy(other.gameObject);
+            Time.timeScale = 0;
+            dieScreen.SetActive(true);
+            dieSound.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Speed")]
+    public float speed = 8; // Speed of player
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))//use the "W" key 
+        {
+            this.transform.position += Vector3.forward * speed * Time.deltaTime; // Move forwards
+        }
+        if (Input.GetKey(KeyCode.A))//use the "A" key 
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime; // Move left
+        }
+        if (Input.GetKey(KeyCode.S))//use the "S" key 
+        {
+            this.transform.position += Vector3.back * speed * Time.deltaTime; // Move backwards
+        }
+        if (Input.GetKey(KeyCode.D))//use the "D" key 
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime; // Move right
+        }
+    }
+}
